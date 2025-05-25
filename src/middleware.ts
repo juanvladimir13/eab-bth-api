50437d0d49238eba6aa6743da9ef377c1cdf681b
Juan Vladimir <juanvladimir13@gmail.com>
2025-05-25 07:59:50 -0400

    support uploads files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,11 @@
 import type { BunRequest } from "bun";
 
+interface FileUpload {
+  uniqueName: string;
+  name: string;
+  file: File;
+}
+
 export const loadData = async (request: BunRequest) => {
   const contentType = request.headers.get("content-type");
 
@@ -19,6 +25,35 @@ export const loadData = async (request: BunRequest) => {
       return Object.fromEntries(dataForm.entries());
     }
     catch (error) {
+      console.error("Error parsing form data:", error);
+      return {};
+    }
+  }
+
+  if (contentType?.includes("multipart/form-data")) {
+    const filesFormData: FileUpload[] = [];
+    const formData = await request.formData()
+    const formDataValues = {};
+
+    try {
+      for (const [key, value] of formData.entries()) {
+        if (value instanceof File) {
+          const arrayBuffer = await value.arrayBuffer();
+          const buffer = Buffer.from(arrayBuffer);
+          const uniqueName = `${Date.now()}-${value.name}`;
+          await Bun.write(`./uploads/${uniqueName}`, buffer);
+          filesFormData.push({
+            name: key,
+            uniqueName: uniqueName,
+            file: value
+          });
+        } else {
+          formDataValues[key] = value;
+        }
+      }
+
+      return { files: filesFormData, formData: formDataValues };
+    } catch (error) {
       console.error("Error parsing form data:", error);
       return {};
     }
